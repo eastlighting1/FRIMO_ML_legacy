3381bac09455729d3a1c624b8869960a1ce98041
eastlighting1 <31691750+eastlighting1@users.noreply.github.com>
2023-03-20 20:39:19 +0900

    Update train.py

--- Model/Style-Change/train.py
+++ Model/Style-Change/train.py
@@ -1,27 +1,60 @@
-from transformers import pipeline
-import argparse
-import pandas as pd
+from transformers import (
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    Seq2SeqTrainingArguments,
+    Seq2SeqTrainer,
+    DataCollatorForSeq2Seq,
+)
 
-def generate_text(pipe, text, target_style, num_return_sequences=5, max_length=60):
-  target_style_name = style_map[target_style]
-  text = f"{target_style_name} 말투로 변환:{text}"
-  out = pipe(text, num_return_sequences=num_return_sequences, max_length=max_length)
-  return [x['generated_text'] for x in out]
+from tokenizers import Tokenizer
+from typing                  import Dict, List, Optional
+from torch.utils.data        import Dataset
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+import numpy as np
+
+
+class TextStyleTransferDataset(Dataset):
+  def __init__(self, 
+               df: pd.DataFrame, 
+               tokenizer: Tokenizer
+               ):
+    self.df = df
+    self.tokenizer = tokenizer
+    
+  def __len__(self):
+    return len(self.df)
+
+  def __getitem__(self, index):
+    row = self.df.iloc[index, :].dropna().sample(2)
+    text1 = row[0]
+    text2 = row[1]
+    target_style = row.index[1]
+    target_style_name = style_map[target_style]
+
+    encoder_text = f"{target_style_name} 말투로 변환:{text1}"
+    decoder_text = f"{text2}{self.tokenizer.eos_token}"
+    model_inputs = self.tokenizer(encoder_text, max_length=64, truncation=True)
+
+    with self.tokenizer.as_target_tokenizer():
+      labels = tokenizer(decoder_text, max_length=64, truncation=True)
+    model_inputs['labels'] = labels['input_ids']
+    del model_inputs['token_type_ids']
+
+    return model_inputs
+
+df = pd.read_csv("C:/Users/User/Desktop/model/smile_style/dataset/smilestyle_dataset.tsv", sep="\t")
 
 model_name = "gogamza/kobart-base-v2"
-model_path = "C:/Users/User/Desktop/model/smile_style/output"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-parser = argparse.ArgumentParser(description='Simsimi based on KoGPT-2')
+row_notna_count = df.notna().sum(axis=1)
+df = df[row_notna_count >= 2]
 
-parser.add_argument('--chat_sentence',
-                    type=str,
-                    default=True,
-                    help='ai chatting data')
+lengths = []
 
-parser.add_argument('--chat_style',
-                    type=str,
-                    default=True,
-                    help='ai chatting style')
+lengths = pd.Series(lengths)
 
 style_map = {
     'formal': '문어체',
@@ -43,12 +76,49 @@ style_map = {
     'translator': '번역기'
 }
 
-nlg_pipeline = pipeline('text2text-generation',model=model_path, tokenizer=model_name)
+dataset = TextStyleTransferDataset(df, tokenizer)
 
-args = parser.parse_args()
+df_train, df_test = train_test_split(df, test_size=0.1, random_state=42)
 
-src_text = args.chat_sentence
-style = args.chat_style
+train_dataset = TextStyleTransferDataset(
+    df_train,
+    tokenizer
+)
+test_dataset = TextStyleTransferDataset(
+    df_test,
+    tokenizer
+)
 
-print("입력 문장:", src_text)
-print("수정 문장 : ", generate_text(nlg_pipeline, src_text, style, num_return_sequences=1, max_length=1000)[0])
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+data_collator = DataCollatorForSeq2Seq(
+    tokenizer=tokenizer, model=model
+)
+
+model_path = "C:/Users/User/Desktop/model/smile_style/output"
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir=model_path, #The output directory
+    overwrite_output_dir=True, #overwrite the content of the output directory
+    num_train_epochs=1, # number of training epochs
+    per_device_train_batch_size=16, # batch size for training
+    per_device_eval_batch_size=16,  # batch size for evaluation
+    eval_steps=500, # Number of update steps between two evaluations.
+    save_steps=1000, # after # steps model is saved 
+    warmup_steps=300,# number of warmup steps for learning rate scheduler
+    prediction_loss_only=True,
+    evaluation_strategy="steps",
+    save_total_limit=3
+    )
+
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=train_dataset,
+    eval_dataset=test_dataset,
+)
+
+trainer.train()
+
+trainer.save_model("C:/Users/User/Desktop/model/smile_style/output")
